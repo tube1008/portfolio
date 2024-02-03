42bf6bc8d3834d967ff00ca196bc85d23fe479ef
tube1008 <ilhyuk0504@gmail.com>
2024-02-04 02:50:04 +0900

    자바만 완성하자

--- script/about.js
+++ script/about.js
@@ -1,18 +1,12 @@
-const title = document.querySelectorAll('.title h1')
-const link_a = document.querySelectorAll('.link a')
+const title = document.querySelectorAll('main > section')
+const link_button = document.querySelectorAll('#btn')
 const menu_a = document.querySelectorAll('.gnb a')
-console.log(title, link_a, menu_a)
+console.log(title, link_button, menu_a)
 
-menu_a.forEach((obj, idx)=>{
-    obj.addEventListener('click',(e)=>{
-        e.preventDefault()
-        window.scrollTo(0, title[idx].offsetTop)
-    })
+link_button[0].addEventListener('click',()=>{
+    console.log(title[0].offsetTop)
+    console.log(title[1].offsetTop)
+    console.log(title[2].offsetTop)
+    console.log(title[3].offsetTop)
+    window.scrollTo(0, title[0].offsetTop)
 })
-
-link_a.forEach((t,i)=>{
-    t.addEventListener('click',(e)=>{
-        e.preventDefault() // 3번째 버튼을 클릭했을때 첫번째로 돌아가는 것을 방지 해준다 (a태그로할시에) button태그로 하면 이런 현상이 없음
-        window.scrollTo(0, title[i].offsetTop)
-    })
-})  
