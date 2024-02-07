b488fdbe87a54df5ab8e3871d35afe360b2946b2
tube1008 <ilhyuk0504@gmail.com>
2024-02-07 22:26:49 +0900

    스크롤 완

--- script/about.js
+++ script/about.js
@@ -33,10 +33,31 @@ link_button[3].addEventListener('click',()=>{
     window.scrollTo(0, title[3].offsetTop)
 })
 
-top_menu[0].addEventListener('click',()=>{
+top_menu[1].addEventListener('click',()=>{
     console.log(title[0].offsetTop)
     console.log(title[1].offsetTop)
     console.log(title[2].offsetTop)
     console.log(title[3].offsetTop)
     window.scrollTo(0, title[0].offsetTop)
+})
+top_menu[2].addEventListener('click',()=>{
+    console.log(title[0].offsetTop)
+    console.log(title[1].offsetTop)
+    console.log(title[2].offsetTop)
+    console.log(title[3].offsetTop)
+    window.scrollTo(0, title[1].offsetTop)
+})
+top_menu[3].addEventListener('click',()=>{
+    console.log(title[0].offsetTop)
+    console.log(title[1].offsetTop)
+    console.log(title[2].offsetTop)
+    console.log(title[3].offsetTop)
+    window.scrollTo(0, title[2].offsetTop)
+})
+top_menu[4].addEventListener('click',()=>{
+    console.log(title[0].offsetTop)
+    console.log(title[1].offsetTop)
+    console.log(title[2].offsetTop)
+    console.log(title[3].offsetTop)
+    window.scrollTo(0, title[3].offsetTop)
 })
