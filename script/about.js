14ca9a78255db8954300997c59c3dd53a6abd39d
Ahn il hyuk <ilhyuk0504@gmail.com>
2024-02-08 17:59:58 +0900

    send mail

--- script/about.js
+++ script/about.js
@@ -4,6 +4,8 @@ const link_button = document.querySelectorAll('#btn')
 const menu_a = document.querySelectorAll('.gnb a')
 console.log(title, link_button, menu_a)
 
+
+// 오른쪽 고정 버튼 
 link_button[0].addEventListener('click',()=>{
     console.log(title[0].offsetTop)
     console.log(title[1].offsetTop)
@@ -33,10 +35,25 @@ link_button[3].addEventListener('click',()=>{
     window.scrollTo(0, title[3].offsetTop)
 })
 
-top_menu[0].addEventListener('click',()=>{
+// 상단 헤더
+menu_a[2].addEventListener('click',()=>{
     console.log(title[0].offsetTop)
     console.log(title[1].offsetTop)
     console.log(title[2].offsetTop)
     console.log(title[3].offsetTop)
-    window.scrollTo(0, title[0].offsetTop)
+    window.scrollTo(0, title[1].offsetTop)
+})
+menu_a[3].addEventListener('click',()=>{
+    console.log(title[0].offsetTop)
+    console.log(title[1].offsetTop)
+    console.log(title[2].offsetTop)
+    console.log(title[3].offsetTop)
+    window.scrollTo(0, title[2].offsetTop)
+})
+menu_a[4].addEventListener('click',()=>{
+    console.log(title[0].offsetTop)
+    console.log(title[1].offsetTop)
+    console.log(title[2].offsetTop)
+    console.log(title[3].offsetTop)
+    window.scrollTo(0, title[3].offsetTop)
 })
